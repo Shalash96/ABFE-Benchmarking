261dad2cde56fa2e0c09342313218603008c162c
Mahmoud Shalash <59184028+Shalash96@users.noreply.github.com>
2022-07-19 15:42:09 +0100

    Update findLIG.sh

--- SimulationAnalysis/hbond/findLIG.sh
+++ SimulationAnalysis/hbond/findLIG.sh
@@ -1,5 +1,5 @@
-extension="dat"
-word="lig"
+extension="dat" # the extension of the files you want to search in
+word="lig"      # the word that you want to search for
 for file in $ls *.$extension
 do
     grep -i "lig" $file > $(basename $file .$extension)_lig.$extension
